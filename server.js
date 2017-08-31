18acb1853d89c5635de9fcd0991cd565b9c256a4
raahuul <er.rahul.v27@gmail.com>
2017-09-01 03:44:33 +0530

    removed index-one.html, declared articleOne named javascript object and a function named createTemplate that takes articleOne js object as input and generates a template html in form of a multiline string and returns it which is then sent to the client as a string

--- server.js
+++ server.js
@@ -5,11 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
+
+var articleOne= {
+  title: 'Article-one! courtesy TOI and ISRO',
+  heading: 'Article-one',
+  date:'Sep 1 2017',
+  content:`<p>
+     This is the content of my first article.The Indian Space Research Organisation's (ISRO) workhorse,
+     the Polar Satellite Launch Vehicle (PSLV), on Thursday failed for the first time in 24 years,
+     after a glitch in its heat shield. This is the only second time in its history that the PSLV had failed.
+     The only failure of the launch vehicle was when the rocket had failed to ignite after the second stage separated on September 20, 1993
+  </p>
+  <p>
+    On Thursday, Isro announced that the heat shield on PSLV-C39 could not open
+    and hence the satellite (IRNSS-1H) could not be released.
+  </p>
+  <p>
+    Polar Satellite Launch Vehicle (PSLV) is the third generation launch vehicle of India. It is the first Indian launch vehicle to be equipped with liquid stages.
+    After its first successful launch in October 1994, PSLV emerged as the reliable and versatile workhorse launch vehicle of India with 39 consecutively successful missions
+    Besides, the vehicle successfully launched two spacecraft – Chandrayaan-1 in 2008 and Mars Orbiter Spacecraft in 2013 – that later traveled to Moon and Mars respectively.
+    IRNSS-1H, the eighth in the NavIC constellation, was to replace IRNSS-1A, the first satellite in the constellation whose rubidium atomic clocks had stopped functioning.
+    The clocks are a critical component in providing accurate locational data.
+  </p>`
+};
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+       <head>
+           <title>
+             ${title}
+           </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+       </head>
+    <body>
+       <div class="container">
+          <div>
+            <a href="/">Home></a>
+          </div>
+          <hr/>
+          <h3>${heading}</h3>
+          <div>
+            ${date}
+          </div>
+          <div>
+            ${content}
+          </div>
+      </div>
+    </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));

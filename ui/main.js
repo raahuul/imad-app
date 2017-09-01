18a07d07367404615c61ce8c998d9f9165fa1dae
raahuul <er.rahul.v27@gmail.com>
2017-09-01 17:16:28 +0530

    changing the content of the element having id=main-text by adding js code in main.js file

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+//changing the content of the element havig id=main-text
+var element = document.getElementById('main-text');
+element.innerHTML=`Hi! I am rahul and this is my first web app.
+<h4>The 3 requests made by this page are:</h4>
+     request style.css file(served),
+     request madi.png file(served),
+     request main.js file(IS NOW SERVED).`;

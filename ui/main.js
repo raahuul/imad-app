3e6481e38be9df8fd37b64e37acdd2aeda7841cb
raahuul <er.rahul.v27@gmail.com>
2017-09-01 17:44:01 +0530

    added a script to move madi if clicked

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,9 @@ element.innerHTML=`Hi! I am rahul and this is my first web app.
      request style.css file(served),
      request madi.png file(served),
      request main.js file(IS NOW SERVED).`;
+
+//move MADI
+var img= document.getElementById('madi');
+img.onclick= function(){
+  img.style.marginLeft= '100px';
+};

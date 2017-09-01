eb970266c4bce5baa5a79165b33d9627c768bd21
raahuul <er.rahul.v27@gmail.com>
2017-09-01 19:43:07 +0530

    implemented a moving madi using setInterval

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,12 @@ element.innerHTML=`Hi! I am rahul and this is my first web app.
 
 //move MADI
 var img= document.getElementById('madi');
-img.onclick= function(){
-  img.style.marginLeft= '100px';
+var marginLeft= 0;
+function moveRight() {
+  marginLeft= marginLeft + 5;
+  img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick= function() {
+  var interval= setInterval(moveRight, 50);
+
 };
